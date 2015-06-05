88ef57bd774d584205116974adc42b36985de335
shycherry <vinpowful@free.fr>
2015-06-05 19:06:30 +0200

    wip

--- app.js
+++ app.js
@@ -19,7 +19,44 @@ app.addEventListener("dom-change", function () {
     this.page = iPageName;
   }.bind(this);
 
-  // switchPage("boggle");
-  switchPage("crosswords");
+  switchPage("boggle");
+  // switchPage("crosswords");
   // switchPage("cesarheart");
 });
+
+var ITNode = require("IT_NODE").ITNode;
+
+var itNode = new ITNode();
+var myport = itNode.port;
+
+function ack(data){
+  console.log(data);
+}
+
+function youdoHandler(err, itSocket){
+  var count = 0;
+
+  itSocket.on("handshake", function(data, cb){
+    cb = cb || function(){};
+    return cb("reçu:"+ JSON.stringify(data));
+  });
+
+  itSocket.emit("handshake", "coucou", ack.bind(this));
+};
+
+itNode.addService({
+  name : "youdo",
+  handler : youdoHandler
+});
+
+function connect(ipport){
+
+  var args = {
+    'node' : ipport,
+    'name':'youdo',
+    'args':['test', 'test2']
+  };
+
+  itNode.callService( args, youdoHandler.bind(this) );
+}
+
