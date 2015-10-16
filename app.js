37f95d56a8bc270862a6889525b32ad8f7d7c7c9
shycherry <vinpowful@free.fr>
2015-10-16 21:54:37 +0200

    wip

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var switchPage = null;
 app.addEventListener("dom-change", function () {
 
   this.$.pages.addEventListener('iron-select', function(ev){
-      ev.detail.item.bindPage();
+    ev.detail.item.bindPage();
   });
 
   this.$.pages.addEventListener('iron-deselect', function(ev){
-      ev.detail.item.unbindPage();
+    ev.detail.item.unbindPage();
   });
 
   this.$.pages.addEventListener('dyw-page-finished', function(ev){
